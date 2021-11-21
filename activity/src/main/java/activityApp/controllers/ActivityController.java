2e8763915c3857d43edb7d17be42634eacad3bb5
Anna Popova <anna.popova201909@gmail.com>
2021-11-21 21:35:02 +0300

    добавила активити контроллер, активитис без тотала возврвщаются

--- activity/src/main/java/activityApp/controllers/ActivityController.java
+++ activity/src/main/java/activityApp/controllers/ActivityController.java
@@ -2,23 +2,29 @@ package activityApp.controllers;
 
 
 import activityApp.dao.ActivityDao;
+import activityApp.domain.Activity;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
-@RequestMapping("/activity")
+@RequestMapping("/activities")
 public class ActivityController {
 
     private final ActivityDao activityDao;
 
     @GetMapping()
-    public String showAll( Model model) {
-        model.addAttribute("activityList", activityDao.findAll());
-        return "activities/showAll";
+    public String getStatsByTrip(@RequestParam(value = "trip_id") long trip_id, Model model) {
+        model.addAttribute("activityList", activityDao.getActivitiesByTripId(trip_id));
+        final List<Activity> activitiesByTripId = activityDao.getActivitiesByTripId(trip_id);
+        System.out.println(" РАСПЕЧАТАН "+ activitiesByTripId.size());
+        return "activities/getStatByTrip";
     }
 
 }
--- activity/src/main/java/activityApp/dao/ActivityDao.java
+++ activity/src/main/java/activityApp/dao/ActivityDao.java
@@ -4,6 +4,10 @@ import activityApp.domain.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public interface ActivityDao extends JpaRepository<Activity,Long> {
+
+    List<Activity> getActivitiesByTripId(long trip_id);
 }
